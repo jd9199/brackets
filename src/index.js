fea9fd83c9d94c89369e11b8818a537357ef1d8c
jd9199 <grafclown999@gmail.com>
2019-03-24 21:53:02 +0300

    Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let masStr = str.split('');
+	let masOfBrackets = [], index = 0;
+		for (let i = 0, masStrLength = masStr.length; i < masStrLength; i++)
+		{
+			for (let j = 0, bracketsConfigLength = bracketsConfig.length; j < bracketsConfigLength; j++)
+			{
+				let firstOfBrackets = bracketsConfig[j][0];
+				let secondOfBrackets = bracketsConfig[j][1];
+				if (masStr[i] === firstOfBrackets)
+				{
+					masOfBrackets[index] = masStr[i];
+					index++;
+					j = bracketsConfigLength;
+				}
+				else if (masStr[i] === secondOfBrackets && firstOfBrackets === masOfBrackets[index-1])
+				{
+					delete masOfBrackets[index-1];
+					index--;
+					j = bracketsConfigLength;
+				}
+				else if (masStr[i] === secondOfBrackets && firstOfBrackets !== masOfBrackets[index-1])
+				{
+					return false;
+				}
+			}
+		}
+		if(masOfBrackets[0])
+		{
+			return false;
+		}
+		return true;
 }
